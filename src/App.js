89789d50d5006626d0665c7b12850659c4adc7a4
Haku123321 <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>
2023-08-31 13:35:27 +0800

    123

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function A1() {
+  return <div>1</div>;
+}
+function A2() {
+  return <div>2</div>;
+}
+function A3() {
+  return <div>3</div>;
+}
+function A4() {
+  return <div>4</div>;
+}
+function A5() {
+  return <div>5</div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" >
+          <Route index element={<A1 />} />
+          <Route path="a1" element={<A1 />} />
+          <Route path="a2" element={<A2 />} />
+          <Route path="a3" element={<A3 />} />
+          <Route path="a4" element={<A4 />} />
+          <Route path="a5" element={<A5 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
